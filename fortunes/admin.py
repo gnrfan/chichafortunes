b911db0aed5932abaf52a50a8712e2619e29b892
Antonio Ognio <gnrfan@gnrfan.org>
2009-09-21 20:20:57 -0500

    Initial filters in the admin

--- fortunes/admin.py
+++ fortunes/admin.py
@@ -6,5 +6,12 @@ from models import Fortune
 
 admin.autodiscover()
 
-# Registramos otros modelos que no encuentre autodiscover()
-admin.site.register(Fortune)
+class FortuneOptions(admin.ModelAdmin):
+    """Custom options for the Fortune model in the admin"""
+    ordering = ('-created_at', 'moderated', )
+    list_display = ('body', 'accepted', 'moderated', 'created_at', )
+    list_filter = ('moderated', 'accepted', 'origin', )
+
+admin.site.register(Fortune, FortuneOptions)
+
+
